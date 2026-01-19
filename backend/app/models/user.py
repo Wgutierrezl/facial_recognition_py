71bc8aa59320c840e50eab62806cc4ceea435058
walterDeveloper <waltergutierrezl123@gmail.com>
2026-01-19 12:13:28 -0500

    we start this project to practice facial rekognition with fast api, aws and then react native

---
+++ backend/app/models/user.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from app.config.db_config import Base
+
+class User(Base):
+
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    face_id= Column(String, unique=True, index=True, nullable=False)
