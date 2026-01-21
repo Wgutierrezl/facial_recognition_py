c40bde74f220caf004a8a6aef7cea5b2d4c20b39
walterDeveloper <waltergutierrezl123@gmail.com>
2026-01-21 11:56:26 -0500

    we started to do the attendance service, controller and others, we validate the first endpoint, that endpoint register the entrance, now we have to test the other endpoint that register the exit

--- backend/app/repositories/attendance_repository.py
+++ backend/app/repositories/attendance_repository.py
@@ -1,19 +1,46 @@
-from sqlalchemy.orm import Session
-from backend.app.models.place import Place
+from typing import List
+from app.models.attendance import Attendance
+from datetime import date
+from sqlalchemy.orm import Session, joinedload
+from app.models.attendance import Attendance
+from app.schemas.attendance_schema import AttendanceResponse
 
 class AttendanceRepository:
 
     def __init__(self, db:Session):
         self.db = db
 
-    def get_place_by_id(self, place_id:int) -> Place:
-        return self.db.query(Place).filter(Place.id == place_id).first()
-    
-    def get_all_places(self) -> list[Place]:
-        return self.db.query(Place).all()
-    
-    def create_place(self, place:Place) -> Place:
-        self.db.add(place)
+    def create_attendance(self, attendance:Attendance) -> AttendanceResponse:
+        self.db.add(attendance)
         self.db.commit()
-        self.db.refresh(place)
-        return place
+        self.db.refresh(attendance)
+        return attendance
+
+    def get_attendance_by_user_id(self, user_id:str) -> List[AttendanceResponse]:
+        return (
+            self.db.query(Attendance)
+                .options(joinedload(Attendance.user), 
+                         joinedload(Attendance.place))
+                .filter(Attendance.user_id == user_id).all()
+        )
+
+    def get_attendance_by_place_id(self, place_id:int) -> List[AttendanceResponse]:
+        return (
+            self.db.query(Attendance)
+            .options(joinedload(Attendance.user), 
+                     joinedload(Attendance.place))
+            .filter(Attendance.place_id == place_id).all()
+        )
+
+    def update_attendance(self, attendance:Attendance) -> AttendanceResponse:
+        self.db.commit()
+        self.db.refresh(attendance)
+        return attendance
+    
+    def get_attendance_today_user_id(self, user_id:str) -> AttendanceResponse:
+        return self.db.query(Attendance).filter(Attendance.user_id==user_id,
+                                                Attendance.work_date==date.today(),
+                                                Attendance.exit_time==None).first()
+    
+    
+    
---
+++ backend/app/services/attendance_service.py
@@ -0,0 +1,94 @@
+from app.models.attendance import Attendance
+from fastapi import UploadFile
+from app.schemas.attendance_schema import AttendanceEntrance, AttendanceResponse
+from app.repositories.attendance_repository import AttendanceRepository
+from sqlalchemy.orm import Session
+from datetime import date, datetime, time, timedelta
+from typing import List
+from app.services.rekognition_service import RekognitionService
+
+class AttendanceService:
+
+    def __init__(self, db:Session):
+        self.db = db
+        self.attendance_repository = AttendanceRepository(self.db)
+        self.rekognition_service = RekognitionService()
+        self.collection_id="users_collection"
+
+    def create_attendance_entrance(self, data:AttendanceEntrance, user_id:str,image:UploadFile) -> AttendanceResponse:
+        try:
+
+            if image is None:
+                raise ValueError('you must to upload a image')
+            
+            image_bytes=image.file.read()
+            
+            face_id=self.rekognition_service.search_face(self.collection_id,
+                                                         image_bytes=image_bytes)
+            
+            if not face_id:
+                raise ValueError('your face doesnt coincide with the facial rekognition')
+            
+            attendance_create=Attendance(
+                user_id=user_id,
+                place_id=data.place_id,
+                work_date=date.today(),
+                entry_time=datetime.now().time(),
+                exit_time=None,
+                total_hours=0,
+                face_verified=face_id
+            )
+
+            attendance_data=self.attendance_repository.create_attendance(attendance_create)
+
+            if not attendance_data:
+                raise ValueError('we cant create the attendance')
+            
+            return attendance_data
+
+        except Exception as e:
+            raise e
+    
+    def create_attendance_exit(self, user_id:str, image:UploadFile) -> AttendanceResponse:
+        try:
+
+            actual_attendance=self.attendance_repository.get_attendance_today_user_id(user_id=user_id)
+
+            if not actual_attendance:
+                raise('you dont check the entrance')
+            
+            image_bytes=image.file.read()
+            
+            facial_id=self.rekognition_service.search_face(self.collection_id,
+                                                           image_bytes=image_bytes)
+            
+            if not facial_id:
+                raise ValueError('we dont rekognition your face id')
+            
+            # we get the actual date that the user leave or get out
+            actual_date=datetime.now().time()
+
+            actual_attendance.exit_time=actual_date
+
+            entry = actual_attendance.entry_time
+            exit = actual_attendance.exit_time
+
+            entry_dt = datetime.combine(date.today(), entry)
+            exit_dt = datetime.combine(date.today(), exit)
+
+            diff = exit_dt - entry_dt
+            actual_attendance.total_hours = round(diff.total_seconds() / 3600, 2)
+
+            attendance_updated=self.attendance_repository.update_attendance(actual_attendance)
+
+            if not attendance_updated:
+                raise ValueError('we cant check your leave')
+            
+            return attendance_updated
+        except Exception as e:
+            raise e
+
+
+
+
+
